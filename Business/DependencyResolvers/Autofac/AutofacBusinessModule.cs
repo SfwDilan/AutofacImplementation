2b2e65f61a62c3c9e47120b7e88e84018b76e5c7
Dilan Budak <dilanbudak04@gmail.com>
2022-09-14 14:21:47 +0300

    Add project files.

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    //Busines > Autofac packages added.
+    // WepApi>Program.cs içerisinde değişiklik yapıldı.
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();  // SingleInstance: Data yok ... Tek bir instance milyonlarca kullanım..
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+        }
+    }
+}
